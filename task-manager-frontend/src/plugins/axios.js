1945023ba001122a14f2d5aa72633575b8350d21
Jakir Hosen <jakir0613@gmail.com>
2025-10-11 19:06:23 +0600

    Backend dockerized

--- task-manager-frontend/src/plugins/axios.js
+++ task-manager-frontend/src/plugins/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
   baseURL: 'http://127.0.0.1:8000/api',
+  // baseURL: 'http://localhost:9000/api',
   // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
   headers: {
     'Content-Type': 'application/json',
